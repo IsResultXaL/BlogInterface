2a7d92d80dea956ba48e9f2bf30813548bec68c7
康良玉 <kangly@game7.cc>
2018-09-25 18:16:41 +0800

    代码规范优化

--- src/main/java/com/caogen/blog/controller/BlogController.java
+++ src/main/java/com/caogen/blog/controller/BlogController.java
@@ -5,13 +5,19 @@ import com.caogen.blog.entity.Blog;
 import com.caogen.blog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
+/**
+ * blog interface
+ * @author kcaogen
+ */
 @RestController
 @RequestMapping("/api/blog")
 public class BlogController {
@@ -21,19 +27,20 @@ public class BlogController {
     @Autowired
     BlogService blogService;
 
-    @GetMapping
-    public InfoResult getBlog() {
-        InfoResult result = null;
+    @GetMapping("/{currentPage}")
+    public InfoResult getBlog(@PathVariable("currentPage") int currentPage) {
+        InfoResult result = new InfoResult(HttpServletResponse.SC_NO_CONTENT, null);
         try {
             List<Blog> blogList = blogService.getBlog();
-            result = new InfoResult(200, blogList);
+            if (blogList!=null && !blogList.isEmpty()) {
+                result = new InfoResult(HttpServletResponse.SC_OK, blogList);
+            }
         } catch (Exception e) {
-            result = new InfoResult(500, "服务器故障！");
+            result = new InfoResult(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "服务器故障！");
             logger.error(e.getMessage());
             e.printStackTrace();
-        } finally {
-            return result;
         }
+        return result;
     }
 
 }
